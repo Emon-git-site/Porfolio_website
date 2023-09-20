b1a0e62b9ef9fc83107edb0c8d0bee4d562abbc5
Emon-git-site <136096636+Emon-git-site@users.noreply.github.com>
2023-09-20 12:08:00 +0600

    desktop version complete

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+let tabLinks =  document.getElementsByClassName("about_tab_link");
+let tabContents = document.getElementsByClassName("about_tab_content");
+
+function opentab(tabname){
+
+    for(let tablink of tabLinks){
+        tablink.classList.remove("active_link");
+     
+    }
+
+    for(let tabcontent of tabContents){
+        tabcontent.classList.remove("active_tab");
+    }
+    event.currentTarget.classList.add("active_link");
+    document.getElementById(tabname).classList.add("active_tab")
+   
+}
