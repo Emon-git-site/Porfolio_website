8ece39e88742295256edf0057e745746242bf64d
Emon-git-site <136096636+Emon-git-site@users.noreply.github.com>
2023-09-23 09:46:27 +0600

    portfolio project done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,42 +1,38 @@
 
 // about tab section function
-let tabLinks =  document.getElementsByClassName("about_tab_link");
+let tabLinks = document.getElementsByClassName("about_tab_link");
 let tabContents = document.getElementsByClassName("about_tab_content");
 
-function opentab(tabname){
+function opentab(tabname) {
 
-    for(let tablink of tabLinks){
+    for (let tablink of tabLinks) {
         tablink.classList.remove("active_link");
-     
+
     }
 
-    for(let tabcontent of tabContents){
+    for (let tabcontent of tabContents) {
         tabcontent.classList.remove("active_tab");
     }
     event.currentTarget.classList.add("active_link");
     document.getElementById(tabname).classList.add("active_tab");
-   console.log(document.getElementById(tabname))
+    console.log(document.getElementById(tabname))
 }
 
 // menu bar open and close system
- menuClose = document.getElementsByClassName("menu_close");
- menuOpen = document.getElementsByClassName("menu_bar");
+menuClose = document.getElementsByClassName("menu_close");
+menuOpen = document.getElementsByClassName("menu_bar")[0]; // Select the first element with the class "menu_bar"
 
-console.log(menuClose);
-console.log(menuOpen);
 
-function closeMenu(){
-   for( close of menuClose){
-    close.style.visibility = "hidden";
-   }
-} 
-   function openMenu(){
-    console.log(menuOpen);
-   
-     for( close of menuClose){
+function closeMenu() {
+    for (close of menuClose) {
+        close.style.visibility = "hidden";
+    }
+    menuOpen.style.display  = "block"; // Show the "menu_open" button
+}
+
+function openMenu() {
+    for (close of menuClose) {
         close.style.visibility = "visible";
-       }
-   
-       menuOpen.style.visibility = "hidden";
-   }
-
+    }
+    menuOpen.style.display  = "none"; // Hide the "menu_open" button
+}
